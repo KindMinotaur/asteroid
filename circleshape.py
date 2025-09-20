6f45d637714c7a2ce5434d6748f712656321b140
KindMinotaur <shaun.belknap@gmail.com>
2025-09-20 13:59:47 -0500

    Added asteroids and finished player movement

--- circleshape.py
+++ circleshape.py
@@ -13,6 +13,8 @@ class CircleShape(pygame.sprite.Sprite):
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
+    def collision(self, ):
+
     def draw(self, screen):
         # sub-classes must override
         pass
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from asteroidfield import *
 from constants import *
 from player import *
 import pygame
@@ -6,23 +7,33 @@ dt = 0
 clock = pygame.time.Clock()
 def main():
     global dt
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    AsteroidField.containers = (updatable,)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     color = (0,0,0)
-    current = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField()
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    print(len(updatable))
+    print(len(drawable))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        current.update(dt)
+        dt = clock.tick(60) / 1000
+        updatable.update(dt)
         pygame.Surface.fill(screen,color)
-        current.draw(screen)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         clock.tick(60)
-        dt = clock.get_time() / 1000
 
 
 
